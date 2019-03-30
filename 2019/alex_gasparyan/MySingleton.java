1732643e9d22bd80ae045483cebafccd0f6467a9
Alex Gasparyanb <alexgasparyan97@gmail.com>
2019-03-30 12:23:43 +0400

    Homework hand in

---
+++ 2019/alex_gasparyan/MySingleton.java
@@ -0,0 +1,14 @@
+public class MySingleton {
+
+    private MySingleton() {
+        
+    }
+
+    private static class Holder {
+        private static MySingleton instance = new MySingleton();
+    }
+
+    public static MySingleton getInstance() {
+        return Holder.instance;
+    }
+}
