2a00c745c959f97b6cedae6f93a34c20abe4946e
la223hd <43174806+la223hd@users.noreply.github.com>
2019-04-26 17:23:34 +0400

    Prototype update

--- 2019/Luiza_Arakelova/Prototype/Phone.cpp
+++ 2019/Luiza_Arakelova/Prototype/Phone.cpp
@@ -8,31 +8,21 @@ public:
 	Phone() {}
 	~Phone() { }
 	virtual Phone *  clone() = 0;
-	virtual void makePhone() = 0;
 };
 
 class SamsungPhone : public Phone {
 	Phone * clone() {
 		return new SamsungPhone(*this);
 	}
-	void makePhone() {
-		cout << "Congrats! Samsung Phone has been made.";
-	}
 };
 class ApplePhone : public Phone {
 	Phone * clone() {
 		return new ApplePhone(*this);
 	}
-	void makePhone() {
-		cout << "Congrats! Apple Phone has been made.";
-	}
 };
 class SonyPhone : public Phone {
 	Phone * clone() {
 		return new SonyPhone(*this);
-	}
-	void makePhone() {
-		cout << "Congrats! Sony Phone has been made.";
 	}
 };
 int main() {
@@ -49,8 +39,6 @@ int main() {
 		phone = prototypes[1]->clone();
 	if (name == "Sony")
 		phone = prototypes[2]->clone();
-
-	phone->makePhone();
 
 	system("pause");
 	return 0;
