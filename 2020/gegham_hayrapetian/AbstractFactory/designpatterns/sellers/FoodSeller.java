2d2d549c281b0bb4efea2e21e4c54f1d2ee1e792
GeghamHayrapetian <49583366+GeghamHayrapetian@users.noreply.github.com>
2020-03-24 03:45:51 +0400

    Add files via upload

---
+++ 2020/gegham_hayrapetian/AbstractFactory/designpatterns/sellers/FoodSeller.java
@@ -0,0 +1,8 @@
+package com.designpatterns.sellers;
+
+public class FoodSeller implements Seller {
+    @Override
+    public void sellGoods() {
+
+    }
+}
---
+++ 2020/gegham_hayrapetian/AbstractFactory/designpatterns/sellers/HouseholdChemistrySeller.java
@@ -0,0 +1,9 @@
+package com.designpatterns.sellers;
+
+public class HouseholdChemistrySeller implements Seller {
+    @Override
+    public void sellGoods() {
+        System.out.println("Hey, I am a household chemistry  seller. How can I help you?");
+
+    }
+}
