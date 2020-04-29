c9f99165940af50b1f8887994623ba1690eb87b7
Hrachya Tandilyan <htandilyan@vmware.com>
2020-04-29 15:23:02 +0400

    - Added initial structure and helper scripts for slides development. - Added sample tex slides for all patterns, based on Proxy pattern. - Currently all pattern are using same motivation and structure images. - Each pattern slides should be refined separately.

---
+++ slides/update_all.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+function update_pattern 
+{
+    PATTERN_TYPE=$1
+    PATTERN_NAME=$2
+    cd $PATTERN_NAME
+    pwd
+    #sed -i "" "s/Proxy/${PATTERN_NAME}/g" tex/$PATTERN_NAME.tex
+    rm Makefile
+    ln -s ../../../Makefile 
+    make clean
+    make slides
+    cp $PATTERN_NAME.pdf ../../../pdf/$PATTERN_TYPE
+    make clean
+    ls -l
+    cd ..
+}
+
+cd src
+
+cd Creational
+update_pattern Creational AbstractFactory
+update_pattern Creational Builder
+update_pattern Creational FactoryMethod
+update_pattern Creational Prototype
+update_pattern Creational Singleton
+cd ..
+
+cd Structural
+update_pattern Structural Adapter
+update_pattern Structural Bridge
+update_pattern Structural Composite
+update_pattern Structural Decorator
+update_pattern Structural Facade
+update_pattern Structural Flyweight
+update_pattern Structural Proxy
+cd ..
+
+cd Behavioral
+update_pattern Behavioral ChainOfResponsibility
+update_pattern Behavioral Command
+update_pattern Behavioral Interpreter
+update_pattern Behavioral Iterator
+update_pattern Behavioral Mediator
+update_pattern Behavioral Memento
+update_pattern Behavioral Observer
+update_pattern Behavioral State
+update_pattern Behavioral Strategy
+update_pattern Behavioral TemplateMethod
+update_pattern Behavioral Visitor
+cd ..
+
