05b10ae8abb39f44e2de9690d20220e91985f88a
Ruben Tumanyan <58356998+rubentumanyan@users.noreply.github.com>
2020-05-09 22:15:05 +0400

    Create Proxy

---
+++ 2020/ruben_tumanyan/Proxy.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+
+class Account_Interface
+{
+public:
+	virtual void cashÎut() = 0;
+	virtual ~Account_Interface() {}
+};
+
+class BankAccount : public Account_Interface
+{
+public:
+	BankAccount() {}
+	void cashÎut() override
+	{
+		std::cout <<  "You've withdrawed 50 dollars\n";
+	}
+};
+
+class AccountProxy : public Account_Interface
+{
+public:
+	AccountProxy(BankAccount& b) : bank_(b), pswd_("hello"), active(false) {}
+	
+	void cashÎut() override
+	{
+		if (active)
+			bank_.cashÎut();
+		else
+			std::cout << "Please authorize\n";
+	}
+
+	void authorize(const std::string& password)
+	{
+		if (pswd_ == password)
+			active = true;
+		else
+			std::cout << "Wrong password, try again\n";
+	}
+private:
+	BankAccount& bank_;
+	std::string pswd_;
+	bool active;
+};
+
+int main()
+{
+	BankAccount bank;
+
+	AccountProxy proxy (bank);
+	proxy.cashÎut();
+	proxy.authorize("123");
+	proxy.authorize("hello");
+	proxy.cashÎut();
+
+	return 0;
+}
