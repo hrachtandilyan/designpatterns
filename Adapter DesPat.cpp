855c5f55025a0b25522d98934195751e53435b9a
David Shekoyan <david.shekoyan@gmail.com>
2020-06-10 03:14:59 +0400

    Adding Adapter Pattern

---
+++ Adapter DesPat.cpp
@@ -0,0 +1,95 @@
+#include <iostream>	 	 
+				
+	 			
+	typedef int Cable;
+		 	
+
+	class EuropeanSocketInterface	 	
+	{	 	
+	    public:	 	
+	        virtual int voltage() = 0;	 	
+		 	
+	        virtual Cable live() = 0;	 	
+	        virtual Cable neutral() = 0; 	 	
+	        virtual Cable earth() = 0;	 	
+	};	 	
+		 	
+	 	
+	class Socket : public EuropeanSocketInterface	 	
+	{	 	
+	    public:	 	
+	        int voltage() { return 230; }	 	
+		 	
+	        Cable live() { return 1; }	 	
+	        Cable neutral() { return -1; }	 	
+	        Cable earth() { return 0; }	 	
+	};	 	
+		 	
+	
+	class USASocketInterface	 	
+	{	 	
+	    public:	 	
+	        virtual int voltage() = 0;	 	
+		 	
+	        virtual Cable live() = 0;	 	
+	        virtual Cable neutral() = 0;	 	
+	};	 	
+		 	
+	
+	class Adapter : public USASocketInterface	 	
+	{	 	
+	    EuropeanSocketInterface* socket;	 	
+		 	
+	    public:	 	
+	        void plugIn(EuropeanSocketInterface* outlet)	 	
+	        {	 	
+	            socket = outlet;	 	
+	        }	 	
+		 	
+	        int voltage() { return 110; }	 	
+	        Cable live() { return socket->live(); }	 	
+	        Cable neutral() { return socket->neutral(); }	 	
+	};	 	
+		 	
+ 	
+	class ElectricKettle	 	
+	{	 	
+	    USASocketInterface* power;	 	
+		 	
+	    public:	 	
+	        void plugIn(USASocketInterface* supply)	 	
+	        {	 	
+	            power = supply;	 	
+	        }	 	
+		 	
+	        void boil()	 	
+	        {	 	
+	            if (power->voltage() > 110)	 	
+	            {	 	
+	                std::cout << "Kettle is on fire!" << std::endl;	 	
+	                return;	 	
+	            }	 	
+		 	
+	            if (power->live() == 1 && power->neutral() == -1)	 	
+	            {	 	
+	                std::cout << "Coffee time!" << std::endl;	 	
+	            }	 	
+	        }	 	
+	};	 	
+		 	
+		 	
+	int main()	 	
+	{	 	
+	    Socket* socket = new Socket;	 	
+	    Adapter* adapter = new Adapter;	 	
+	    ElectricKettle* kettle = new ElectricKettle;	 	
+		 	
+
+	    adapter->plugIn(socket);	 	
+	    kettle->plugIn(adapter);	 	
+		 	
+
+	    kettle->boil();	 	
+		 	
+	    return 0;	 	
+	}	 	
