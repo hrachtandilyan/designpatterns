bd983f7d643d4d731bfd77dfdc10abe830a74f29
Vazgen Safaryan <vazgensafaryan@yandex.com>
2020-06-09 16:58:38 +0400

    Abstract factory, Prototype, Singleton patterns

---
+++ 2020/vazgen_safaryan/Singleton pattern/Singleton pattern.cpp
@@ -0,0 +1,46 @@
+﻿// Singleton pattern.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+
+class Singleton
+{
+private:
+	/* Here will be the instance stored. */
+	static Singleton* instance;
+
+	/* Private constructor to prevent instancing. */
+	Singleton();
+
+public:
+	/* Static access method. */
+	static Singleton* getInstance();
+};
+
+/* Null, because instance will be initialized on demand. */
+Singleton* Singleton::instance = 0;
+
+Singleton* Singleton::getInstance()
+{
+	if (instance == 0)
+	{
+		instance = new Singleton();
+	}
+
+	return instance;
+}
+
+Singleton::Singleton()
+{}
+
+int main()
+{
+	//new Singleton(); // Won't work
+	Singleton* s = Singleton::getInstance(); // Ok
+	Singleton* r = Singleton::getInstance();
+
+	/* The addresses will be the same. */
+	std::cout << s << std::endl;
+	std::cout << r << std::endl;
+}
